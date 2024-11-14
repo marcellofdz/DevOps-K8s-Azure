8eef10572815fb3ec83f24c4ecbdd0725e1a273e
marcellofdz <marcellofdz@ip-192-168-8-103.us-west-2.compute.internal>
2024-11-14 16:22:16 -0400

    Feature: adding pipelines definitions

--- provider.tf
+++ provider.tf
@@ -1,4 +1,16 @@
+terraform { 
+  cloud { 
+    organization = "DevOpsRD" 
+    workspaces { 
+      name = "devops-HCP" 
+    } 
+  } 
+}
 provider "azurerm" {
   features {}
-  subscription_id = "ac11afc9-b35e-44d2-b352-b48d48a51eb6"
+
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+  tenant_id       = var.ARM_TENANT_ID
 }
