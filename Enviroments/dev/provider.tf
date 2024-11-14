c6d51c0dded98918ea20dd0dd27b1a3b13320675
marcellofdz <marcellofdz@ip-192-168-8-103.us-west-2.compute.internal>
2024-11-14 15:55:48 -0400

    adding credentials to provider

--- Enviroments/dev/provider.tf
+++ Enviroments/dev/provider.tf
@@ -9,4 +9,9 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = "ac11afc9-b35e-44d2-b352-b48d48a51eb6"
+
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  tenant_id       = var.ARM_TENANT_ID
+
 }
